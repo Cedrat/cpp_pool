d346fcc34823da525703544883aeab0c44b124de
Cedrat <lnoaille@student.fr>
2021-04-19 15:56:05 +0200

    disable the alignement memory

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,28 +1,59 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#pragma pack(1)
 
+typedef struct s_data{
+    std::string part1;
+    int nb;
+    std::string part2;
+} t_data;
+
+std::string random_name()
+{
+    char keyboard[] = "qwertyuiopasdfghjklzxcvbnm";
+    int i;
+    std::string random;
+    i = 0;
+    while (i < 8)
+    {
+        random[i] += keyboard[std::rand()%26];
+        i++;
+    }
+    return (&random[0]);
+
+}
 void *serialize(void)
 {
-    std::string part1 = "beggini";
-    std::string part2 = "endenden";
-    void *temp;
-    int nb;
-    nb = 20;
-    //void *data = reinterpret_cast<void *>(&nb);
-    std::string sequence;
-    //void *data = reinterpret_cast<void *>(&part1) + reinterpret_cast<void *>(&nb);
-    temp = reinterpret_cast<void *>(&nb);
-    sequence = *reinterpret_cast<std::string *>(&part1)  + *reinterpret_cast<std::string *>(&temp) + *reinterpret_cast<std::string *>(&part2);
-    //std::string part2 = "endingen";
-    void *data = reinterpret_cast<void *>(&sequence);
+    void * void_p;
+    t_data *data = new t_data;
+    
+    data->part1 = random_name();
+    data->nb = std::rand();
+    data->part2 = random_name();
+
+    void_p = reinterpret_cast<void*>(data);
+    return (void_p);
+}
+
+t_data * deserialize(void * raw)
+{
+    t_data *data;
+    data = reinterpret_cast<t_data*>(raw); 
     return (data);
 }
 
 int main()
 {
-    std::string part2;
-    int nb = 0;
-    part2 = *reinterpret_cast<std::string *>(serialize());
-    std::cout << part2 << std::endl;
+    void *seria;
+    t_data *data;
+
+    std::srand(time(0));
+    seria = serialize();
+    data = deserialize(seria);
+ 
+    std::cout << data->part1 << std::endl;
+    std::cout << data->nb << std::endl;
+    std::cout << data->part2 << std::endl;
+    std::cout << sizeof(*data) << std::endl;
 }
