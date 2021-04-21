2271009843e6af36701cdeea74c5083ce0882e5f
Cedrat <lnoaille@student.fr>
2021-04-21 16:25:31 +0200

    ex02 : finish

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -5,9 +5,13 @@ class Array
     public :
         Array<T>();
         Array<T>(unsigned int n);
+        Array<T>(Array<T> const &rhs);
+        ~Array<T>();
         
+        unsigned int size() const;
         T &operator[](unsigned int n);
         const T & operator[](unsigned int n) const;
+        Array<T> & operator=(Array<T> const & rhs);
     
     private : 
         T* _array;
@@ -15,27 +19,79 @@ class Array
 };
 
 template <typename T>
-Array<T>::Array()
+Array<T>::Array() : _size(0)
 {
-    _array = new T[0]; 
+    _array = NULL;
 };
 
 template <typename T>
-Array<T>::Array(unsigned int n)
+Array<T>::Array(Array<T> const & rhs)
 {
+    *this = rhs;
+};
+
+template <typename T>
+Array<T>::Array(unsigned int n) :_size(n)
+{
+    if (n == 0)
+    {
+        _array = NULL;
+        return ;
+    }
     _array = new T[n];
     for (unsigned int i = 0; i < n; i++)
-        _array[i] = 0;
+         _array[i] = T();
 };
+
+template <typename T>
+Array<T>::~Array()
+{
+    if (this->size() != 0)
+        delete [] this->_array;
+};
+
 
 template <typename T>
 const T & Array<T>::operator[](unsigned int n) const
 {
+    if (n >= _size)
+    {
+        throw (std::exception());
+    }
     return (_array[n]); 
 };
 
 template <typename T>
- T & Array<T>::operator[](unsigned int n)
+T & Array<T>::operator[](unsigned int n)
 {
+    if (n >= _size)
+    {
+        throw (std::exception());
+    }
     return (_array[n]); 
+};
+
+template <typename T>
+unsigned int Array<T>::size() const
+{
+    return (_size); 
+};
+
+
+template <typename T>
+Array<T> & Array<T>::operator=(Array<T> const & rhs)
+{
+    if (this->size() != 0)
+        delete [] this->_array;
+    if (rhs.size() == 0)
+        this->_array = NULL;
+    else
+        this->_array = new T[rhs.size()];
+
+    for (unsigned int i = 0; i < rhs.size(); i++)
+    {
+        this->_array[i] = rhs._array[i];
+    }
+    this->_size = rhs.size();
+    return (*this);
 };
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,14 +1,63 @@
 #include <iostream>
 #include <string>
 #include "Array.hpp"
+#include<array>
+
+template<typename T>
+void    print_tab(T const & rhs, unsigned int size)
+{
+    for (unsigned  int i = 0 ; i < size ; i++)
+    {
+        std::cout << rhs[i] << std::endl;
+    }
+}
 
 int main()
 {
-    Array<int> empty;
-    Array<int> full(5);
-    int *_array = new int[5]; 
-    std::cout << empty[0] << std::endl;
-    full[0] = 235;
-    std::cout << full[0] << std::endl;
-    std::cout << _array[0] << std::endl;
+    std::cout << "Array of string" << std::endl;
+    Array<std::string> first_name(3);
+    first_name[0] = "Lea";
+    first_name[1] = "Matthieu";
+    first_name[2] = "Lou";
+
+    print_tab<Array<std::string> >(first_name, first_name.size());
+
+    std::cout << "\nArray of int" << std::endl;
+    Array<int> integer(3);
+    integer[0] = 10;
+    integer[1] = 100;
+    integer[2] = 1000;
+
+    print_tab<Array<int> >(integer, integer.size());
+
+
+    std::cout << "\nArray of float" << std::endl;
+    Array<float> floaty(3);
+    floaty[0] = 10.1;
+    floaty[1] = 100.1;
+    floaty[2] = 1000.1;
+
+    print_tab<Array<float> >(floaty, floaty.size());
+
+    std::cout << "\nCreation of an array with size 0" << std::endl;
+    Array<char> letter;
+
+    std::cout << "copy constructor with the array of names" << std::endl;
+    Array<std::string> copy_name(first_name);
+    std::cout << "\nMatthieu become Mat" << std::endl;
+    copy_name[1] = "Mat";
+    std::cout << "\nprint copy name" << std::endl;
+    print_tab<Array<std::string> >(copy_name, copy_name.size());
+    std::cout << "\nreprint the first array" << std::endl;
+    print_tab<Array<std::string> >(first_name, first_name.size());
+    std::cout << "\nsize of the array is " << first_name.size() << std::endl;
+    std::cout << "\ntrying to access slot 4" << std::endl;
+    try
+    {
+        copy_name[4];
+    }
+    catch (std::exception const &e)
+    {
+        std::cout << "Exception catch" << std::endl;
+    }
 }
