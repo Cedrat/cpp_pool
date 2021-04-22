b08cee2b94c9510d1a204026906c0003ab942537
Cedrat <lnoaille@student.fr>
2021-04-22 18:24:02 +0200

    :sparkles: Ex01 begin

---
+++ CPP08/ex01/Span.hpp
@@ -0,0 +1,13 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <algorithm>
+
+class Span
+{
+    Span(unsigned int N);
+    Span(Span const & rhs);
+    ~Span();
+
+    Span & operator=(Span const &rhs)
+}
